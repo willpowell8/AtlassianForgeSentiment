1f71fbe51123a153b9471a41a7bb3dd4b2a199a0
Will Powell <willpowell8@gmail.com>
2020-07-08 20:09:13 +0100

    update to add comment sentiment

--- src/index.jsx
+++ src/index.jsx
@@ -16,10 +16,19 @@ function process(review){
   return sentiment.analyze(review);
 }
 
+
 const fetchForIssue = async (issueId) => {
   const res = await api
     .asApp()
     .requestJira(`/rest/api/3/issue/${issueId}`);
+  const data = await res.json();
+  return data;
+};
+
+const fetchCommentsIssue = async (issueId) => {
+  const res = await api
+    .asApp()
+    .requestJira(`/rest/api/3/issue/${issueId}/comment`);
   const data = await res.json();
   return data;
 };
@@ -29,8 +38,7 @@ const App = () => {
   const [isOpen, setOpen] = useState(false);
   
   const issue = useState(async () => await fetchForIssue(context.platformContext.issueKey));
-  var analysis;
-
+  var count = 0
   var rows = [];
   if(issue[0].fields.description != null) {
     var description = issue[0].fields.description;
@@ -51,40 +59,20 @@ const App = () => {
         var finalText = text.join("\n")
         bodyElements.push(finalText);
         console.log(finalText);
-        analysis = process(finalText);
-        var words = [];
-        analysis.positive.forEach(pos => {
-          var newBodyElements = []
-          bodyElements.forEach(function(item){
-            if(typeof item === 'string' || item instanceof String){
-            var splitElement = item.split(pos)
-            if(splitElement.length > 1){
-              for(var i = 0; i<splitElement.length; i++){
-                newBodyElements.push(splitElement[i]);
-                if(i<(splitElement.length-1)){
-                  newBodyElements.push(<Lozenge text={pos} appearance="success" />)
-                }
-              }
-            }else{
-              newBodyElements.push(item)
-            }
-          }else{
-            newBodyElements.push(item)
-          }
-          })
-          bodyElements = newBodyElements
-          words.push(<Lozenge text={pos} appearance="success" />)
-        })
-        analysis.negative.forEach(pos => {
-          var newBodyElements = []
-          bodyElements.forEach(function(item){
-            if(typeof item === 'string' || item instanceof String){
+        if(finalText.length > 0){
+          var analysis = process(finalText);
+          if(analysis != null){
+            var words = [];
+          analysis.positive.forEach(pos => {
+            var newBodyElements = []
+            bodyElements.forEach(function(item){
+              if(typeof item === 'string' || item instanceof String){
               var splitElement = item.split(pos)
               if(splitElement.length > 1){
                 for(var i = 0; i<splitElement.length; i++){
                   newBodyElements.push(splitElement[i]);
                   if(i<(splitElement.length-1)){
-                    newBodyElements.push(<Lozenge text={pos} appearance="removed" />)
+                    newBodyElements.push(<Lozenge text={pos} appearance="success" />)
                   }
                 }
               }else{
@@ -93,24 +81,47 @@ const App = () => {
             }else{
               newBodyElements.push(item)
             }
+            })
+            bodyElements = newBodyElements
+            words.push(<Lozenge text={pos} appearance="success" />)
           })
-          bodyElements = newBodyElements
-          words.push(<Lozenge text={pos} appearance="removed" />)
-        })
-        rows.push((<Row>
-          <Cell><Text>Description</Text></Cell>
-          <Cell><Text>{analysis.score}</Text></Cell>
-          <Cell><Text>{analysis.comparative}</Text></Cell>
-          <Cell><Text>{words}</Text></Cell>
-        </Row>))
+          analysis.negative.forEach(pos => {
+            var newBodyElements = []
+            bodyElements.forEach(function(item){
+              if(typeof item === 'string' || item instanceof String){
+                var splitElement = item.split(pos)
+                if(splitElement.length > 1){
+                  for(var i = 0; i<splitElement.length; i++){
+                    newBodyElements.push(splitElement[i]);
+                    if(i<(splitElement.length-1)){
+                      newBodyElements.push(<Lozenge text={pos} appearance="removed" />)
+                    }
+                  }
+                }else{
+                  newBodyElements.push(item)
+                }
+              }else{
+                newBodyElements.push(item)
+              }
+            })
+            bodyElements = newBodyElements
+            words.push(<Lozenge text={pos} appearance="removed" />)
+          })
+          count += analysis.score
+          rows.push((<Row>
+            <Cell><Text>Description</Text></Cell>
+            <Cell><Text>{analysis.score}</Text></Cell>
+            <Cell><Text>{analysis.comparative.toFixed(4)}</Text></Cell>
+            <Cell><Text>{words}</Text></Cell>
+          </Row>))
+          }
+        }
         break;
     }
   }
   var titleElements = [];
-  if(issue[0].fields.summary != null){
+  if(issue[0].fields.summary != null && issue[0].fields.summary.length > 0){
     var title = issue[0].fields.summary;
-    console.log(issue[0]);
-    
     titleElements.push(title);
     var titleAnalysis = process(title);
     var words = [];
@@ -158,31 +169,124 @@ const App = () => {
           titleElements = newTitleElements
       words.push(<Lozenge text={pos} appearance="removed" />)
     })
+    count += analysis.score
     rows.push((<Row>
       <Cell><Text>Summary</Text></Cell>
       <Cell><Text>{titleAnalysis.score}</Text></Cell>
-      <Cell><Text>{titleAnalysis.comparative}</Text></Cell>
+      <Cell><Text>{titleAnalysis.comparative.toFixed(4)}</Text></Cell>
       <Cell><Text>{words}</Text></Cell>
     </Row>))
   }
-  //http://willpowell.co.uk/jira/negative.png
-  //http://willpowell.co.uk/jira/neutral.png
-  //http://willpowell.co.uk/jira/positive.png
-  //http://willpowell.co.uk/jira/slightlynegative.png
-  //http://willpowell.co.uk/jira/slightlypositive.png
-  if(analysis != null){
+
+  const commentsData =  useState(async () => await fetchCommentsIssue(context.platformContext.issueKey));
+  console.log("COMMENTS1")
+  console.log(commentsData)
+  if(commentsData[0]['comments'] != null && commentsData[0]['comments'].length > 0){
+    console.log("COMMENTS2")
+    let comments = commentsData[0]['comments']
+    var parts = []
+    comments.forEach(function(comment){
+      var body = comment["body"]["content"]
+      body.forEach(function(bodyPart){
+        console.log(bodyPart)
+        if(bodyPart["content"] != null && bodyPart["content"].length > 0){
+          var bodyPartContent = bodyPart["content"]
+          bodyPartContent.forEach(function(c){
+            if(c["text"] != null && c["text"].length > 0){
+              parts.push(c["text"])
+            }
+          })
+          
+        }
+        
+      })
+    })
+    var str = parts.join(" ");
+    var titleAnalysis = process(str);
+    var words = [];
+    titleAnalysis.positive.forEach(pos => {
+      var newTitleElements = []
+          titleElements.forEach(function(item){
+            if(typeof item === 'string' || item instanceof String){
+              var splitElement = item.split(pos)
+              if(splitElement.length > 1){
+                for(var i = 0; i<splitElement.length; i++){
+                  newTitleElements.push(splitElement[i]);
+                  if(i<(splitElement.length-1)){
+                    newTitleElements.push(<Lozenge text={pos} appearance="success" />)
+                  }
+                }
+              }else{
+                newTitleElements.push(item)
+              }
+            }else{
+              newTitleElements.push(item)
+            }
+          })
+          titleElements = newTitleElements
+      words.push(<Lozenge text={pos} appearance="success" />)
+    })
+    titleAnalysis.negative.forEach(pos => {
+      var newTitleElements = []
+          titleElements.forEach(function(item){
+            if(typeof item === 'string' || item instanceof String){
+              var splitElement = item.split(pos)
+              if(splitElement.length > 1){
+                for(var i = 0; i<splitElement.length; i++){
+                  newTitleElements.push(splitElement[i]);
+                  if(i<(splitElement.length-1)){
+                    newTitleElements.push(<Lozenge text={pos} appearance="removed" />)
+                  }
+                }
+              }else{
+                newTitleElements.push(item)
+              }
+            }else{
+              newTitleElements.push(item)
+            }
+          })
+          titleElements = newTitleElements
+          count += analysis.score/2
+      words.push(<Lozenge text={pos} appearance="removed" />)
+    })
+    rows.push((<Row>
+      <Cell><Text>Comments</Text></Cell>
+      <Cell><Text>{titleAnalysis.score}</Text></Cell>
+      <Cell><Text>{titleAnalysis.comparative.toFixed(4)}</Text></Cell>
+      <Cell><Text>{words}</Text></Cell>
+    </Row>))
+  }
+  
+
+
+  var image = "http://willpowell.co.uk/jira/neutral.png"
+  if(count > 6){
+    image = "http://willpowell.co.uk/jira/positive.png"
+  }else if(count >= 2){
+    image = "http://willpowell.co.uk/jira/slightlypositive.png"
+  }else if(count >= -2) {
+    image = "http://willpowell.co.uk/jira/neutral.png"
+  }
+  else if(count >= -6) {
+    image = "http://willpowell.co.uk/jira/slightlynegative.png"
+  }else{
+    image = "http://willpowell.co.uk/jira/negative.png"
+  }
+  if(rows.length > 0){
     bodyElements.push(" ");
     titleElements.push(" ");
     return (
       <Fragment>
-        <Image src="http://willpowell.co.uk/jira/negative.png" alt="homer"/>
-        <Text>Score {analysis.score}</Text>
+        <Image src={image} alt="Sentiment Score"/>
         <Button
         text={`View Breakdown`}
         onClick={() => setOpen(true)}
       />
       {isOpen && (
         <ModalDialog header="Ticket Sentiment" onClose={() => setOpen(false)}>
+          <Text>{titleElements}</Text>
+          <Text>{bodyElements}</Text>
+          <Text> </Text>
           <Table>
             <Head>
               <Cell><Text>Area</Text></Cell>
@@ -192,8 +296,6 @@ const App = () => {
             </Head>
             {rows}
           </Table>
-          <Text>{titleElements}</Text>
-          <Text>{bodyElements}</Text>
         </ModalDialog>
       )}
       </Fragment>
